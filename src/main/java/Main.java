0df3ab5cbfd7abbfd40eb8d7af477b7361531690
valentincraciun99 <valentin.craciun99@gmail.com>
2020-05-18 10:27:24 +0300

    add Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,26 @@
+import java.sql.*;
 public class Main {
     public static void main(String[] args)
     {
+        try{
+            String user = "root";
+            String password = "";
+            Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/nts",user,password);
+
+            var statement = connection.prepareCall("{call add_user(?,?)}");
+            statement.setString(1,"mail");
+            statement.setInt(2,2);
+
+            statement.execute();
+
+
+            boolean execute = statement.execute("{call add_user()}");
+
+
+        }
+        catch (Exception ex){
+
+        }
 
     }
 }
