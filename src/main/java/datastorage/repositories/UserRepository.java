012d2268f4857b77d1d8f46aa2b90b8f5d45b1d0
valentincraciun99 <valentin.craciun99@gmail.com>
2020-05-23 00:43:13 +0300

    Modify UserRepository

---
+++ src/main/java/datastorage/repositories/UserRepository.java
@@ -0,0 +1,47 @@
+package datastorage.repositories;
+
+import datastorage.DbConnection;
+import model.User;
+import model.UserRole;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class UserRepository extends BaseRepository {
+
+
+    public UserRepository() throws SQLException {
+        super(DbConnection.getInstance());
+    }
+
+    public User GetUser (String user, String password) throws SQLException {
+        var params = new ArrayList<Object>();
+        params.add(user);
+        params.add(password);
+
+        var resultSet = CallStoredProcedure("{call get_user_by_email_and_password(?,?)}", params);
+        resultSet.next();
+
+        return new User(
+                resultSet.getInt(1),
+                UserRole.values()[resultSet.getInt(2)],
+                resultSet.getString(3),
+                resultSet.getString(4),
+                resultSet.getString(5),
+                resultSet.getInt(6));
+
+
+
+        /*while (resultSet.next()) {
+            users.add(new User(
+                    resultSet.getInt(1),
+                    resultSet.getString(2),
+                    resultSet.getString(3)
+            ));
+        */
+
+    }
+
+
+
+}
