18bc315d4cec1069b2295f778ba3fbbb8ab06d6c
valentincraciun99 <valentin.craciun99@gmail.com>
2020-05-23 18:22:09 +0300

    add Node model

--- src/main/java/model/Node.java
+++ src/main/java/model/Node.java
@@ -2,6 +2,49 @@ package model;
 
 public class Node {
     Integer id;
-    Integer node_to;
-    Integer node_from;
+    String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getX() {
+        return x;
+    }
+
+    public void setX(Integer x) {
+        this.x = x;
+    }
+
+    public Integer getY() {
+        return y;
+    }
+
+    public void setY(Integer y) {
+        this.y = y;
+    }
+
+    public Integer getConfigId() {
+        return configId;
+    }
+
+    public void setConfigId(Integer configId) {
+        this.configId = configId;
+    }
+
+    Integer x;
+    Integer y;
+    Integer configId;
 }
